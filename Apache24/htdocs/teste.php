cb32d18da3509885e8d0665d58550f1364637ea5
P3RN4deF3RR0 <davi.carmac@gmail.com>
2024-09-11 17:18:55 -0300

    servidor

---
+++ Apache24/htdocs/teste.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// Mostra todas as informações, equivalente ao padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações sobre módulos.
+// phpinfo(8) gera resultados idênticos.
+phpinfo(INFO_MODULES);
+
+echo "teste"
+
+?>
