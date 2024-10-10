24c0925994a4d20b72776b2df2ba6d49f50a3579
MeckyM <davi.carmac@gmail.com>
2024-10-10 17:14:19 -0300

    front login em desenvolvimento

--- Apache24/htdocs/PHP/index.php
+++ Apache24/htdocs/PHP/index.php
@@ -1,27 +1,36 @@
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
     <title>PHP</title>
 </head>
+
 <body>
-<section class="container">
-    <form method="post" action="script.php">
-    <div class="main-login">
-        <img src="./src/Atenx 1.png">
-        <div class="textfield">
-            <label for="usuario">Usuário</label>
-            <input type="text" name="usuario" placeholder="Usuário">
-        </div>
-        <div class="textfield">
-            <label for="senha">Senha</label>
-            <input type="password" name="senha" placeholder="Senha">
-        </div>
-        <button class="btn" type="submit" name="Entrar" value="Entrar">Confirmar</button>
-    </div>
-    <footer></footer>
-</section>
-</form>
+    <section class="container">
+        <form method="post" action="script.php">
+            <div class="main-login">
+                <img src="./src/Atenx 1.png">
+
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type="text" name="usuario" placeholder="Usuário">
+                </div>
+                <div class="textfield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha">
+                </div>
+                <div class="buttons">
+                    <div class="login">
+                        <button class="btn-login" type="submit" name="Entrar" value="Entrar">Entrar</button>
+                        <button class="btn-cadastro" type="submit" name="Cadastro" value="Entrar">Cadastre-se</button>
+                    </div>
+                </div>
+            </div>
+            <footer></footer>
+    </section>
+    </form>
 </body>
+
 </html>
